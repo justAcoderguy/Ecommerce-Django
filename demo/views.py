9c4f04ee40e045fd7a0246355467fb992c4ede87
Abhishek Banerjee <abhishek.banerjee18@outlook.com>
2022-07-16 21:31:37 +0530

    Added some front end templates to see if orm is working

--- demo/views.py
+++ demo/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
+from inventory import models
 
 
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'index.html')
+
+
+def category(request):
+
+    data = models.Category.objects.all()
+
+    return render(request, 'category.html', {"data": data})
+
+
+def product_by_category(request, category):
+
+    data = models.Product.objects.filter(category__slug=category).values(
+        "id", "name", "slug", "category__name", "product__store_price",
+        "product__product_inventory__units"
+    )
+
+    return render(request, 'product_by_category.html', {"data": data})
+
+
+def product_detail(request, slug):
+
+    data = models.ProductInventory.objects.filter(product__slug=slug).values(
+        "id", "product__name", "sku", "store_price", "product_inventory__units"
+    )
+
+    return render(request, 'product_detail.html', {"data": data})
--- main/tests/fixtures.py
+++ main/tests/fixtures.py
@@ -22,6 +22,7 @@ def django_db_fixture_setup(django_db_setup, django_db_blocker):
         call_command("loaddata", "db_admin_fixture.json")
         call_command("loaddata", "db_category_fixture.json")
         call_command("loaddata", "db_product_fixture.json")
+        call_command("loaddata", "db_category_product_fixture.json")
         call_command("loaddata", "db_brand_fixture.json")
         call_command("loaddata", "db_type_fixture.json")
         call_command("loaddata", "db_product_inventory_fixture.json")
